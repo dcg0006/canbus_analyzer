42966f590a26ffd019e56d0767e753d33e4ac232
hwlewter <hwlewter@gmail.com>
2016-09-09 23:38:20 -0500

    Hwlewter patch 1 (#1)
    * Add files via upload
    
    Uploaded a test file to ensure we can use github properly and see each others submissions and changes.
    
    * Update git_test.cpp
    
    * Update git_test.cpp

---
+++ git_test.cpp
@@ -0,0 +1,14 @@
+// 09/07/16 Github test file
+#include <iostream>
+using namespace std;
+int main(){
+	cout << "hello Github" << endl;
+	cout << "I'd like a few functions to be in our final project: \n";
+	cout << "\t1: displayCaptureWindow1( filename ); //display one file in 1st window\n";
+	cout << "\t2: displayCaptureWindow2( filename ); //display file in 2nd window\n";
+	cout << "\t3: filterOutNonUnique(); //filters out all ID's that are present\n\t\t in both displays\n";
+	cout << "\t4: addContentsToMainCollection( filename ); //adds any new unique IDs to the main\n\t\t collection if not already there\n";
+	cout << "\t5: displayUnknownIDs( filename ); //displays IDs in file that are not defined\n\t\t or present in main collection\n";
+	return 0;
+}
+//end file
