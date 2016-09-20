d8469aa18f86e6bbd9312305161f3b3d675aa90f
hwlewter <hwlewter@gmail.com>
2016-09-20 01:38:38 -0500

    Basic QSqlTable Display (#5)
    filtering functions not implemented yet
    
    * Displays SQL Tables (#3)
    
    * Add files via upload
    
    * Delete canbusAnalyzer.pro.user
    
    * Add files via upload
    
    fixed TEMP tables

---
+++ canbusAnalyzer.pro
@@ -0,0 +1,18 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2016-09-09T22:39:09
+#
+#-------------------------------------------------
+
+QT       += core gui sql widgets
+
+TARGET = canbusAnalyzer
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        mainwindow.cpp
+
+HEADERS  += mainwindow.h
+
+FORMS    += mainwindow.ui
---
+++ mainwindow.h
@@ -0,0 +1,46 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QFile>
+#include <QDebug>
+#include <QSqlTableModel>
+#include <QTableView>
+#include <QSqlQuery>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+void setName(const QString &name);
+
+
+private:
+    Ui::MainWindow *ui;
+    void createMenus();
+    void createActions();
+    void createTempTable(int currentView);
+    QAction *openF1;
+    QAction *openF2;
+    QMenu *file;
+    QTableView *view1;
+    QTableView *view2;
+    QSqlTableModel *tableModel1;
+    QSqlTableModel *tableModel2;
+    int currentView;
+
+private slots:
+    void fOpen1();
+    void fOpen2();
+
+};
+
+#endif // MAINWINDOW_H
